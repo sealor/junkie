367ed93ffd39c53cdedea2454a17e83f69e27561
Stefan Richter <sealor@users.noreply.github.com>
2020-03-29 21:32:55 +0200

    update setup.py for release 2.0.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='junkie',
-    version='1.0.0',
+    version='2.0.0',
     packages=setuptools.find_packages(exclude="test"),
     author='Stefan Richter',
     description='A dependency injection library for beginners',
