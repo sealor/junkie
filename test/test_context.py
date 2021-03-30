fe02dddfd1065667ff625737e8e6f9ab9ccd295c
Stefan Richter <sealor@users.noreply.github.com>
2021-03-30 21:08:16 +0200

    Remove Context.build() variants

--- test/test_context.py
+++ test/test_context.py
@@ -32,49 +32,6 @@ class ContextTest(unittest.TestCase):
         with context.build("value") as value:
             self.assertEqual("abc", value)
 
-    def test_all_possible_build_usages(self):
-        class MyApp:
-            def __init__(self, var1):
-                self.var1 = var1
-
-        context = Context({"var1": "text"})
-
-        with context.build({"A": "var1", "B": MyApp}) as instance_dict:
-            self.assertEqual("text", instance_dict["A"])
-            self.assertEqual("text", instance_dict["B"].var1)
-
-        with context.build(A="var1", B=MyApp) as instance_dict:
-            self.assertEqual("text", instance_dict["A"])
-            self.assertEqual("text", instance_dict["B"].var1)
-
-        with context.build(("var1", MyApp)) as instance_tuple:
-            self.assertEqual("text", instance_tuple[0])
-            self.assertEqual("text", instance_tuple[1].var1)
-
-        with context.build("var1", MyApp) as (instance1, instance2):
-            self.assertEqual("text", instance1)
-            self.assertEqual("text", instance2.var1)
-
-        with context.build("var1") as instance:
-            self.assertEqual("text", instance)
-
-    def test_raise_exception_for_wrong_build_usage(self):
-        class MyApp:
-            pass
-
-        context = Context({"var1": "text"})
-
-        with self.assertRaises(Exception):
-            context.build("var1", B=MyApp)
-
-        with self.assertRaises(Exception):
-            with context.build(["var1"], [MyApp]):
-                pass
-
-        with self.assertRaises(Exception):
-            with context.build({"var1", MyApp}):
-                pass
-
     @skipIf(sys.version_info < (3, 7), "@dataclass needs at least Python 3.7")
     def test_dataclass_decorator(self):
         exec(textwrap.dedent("""
