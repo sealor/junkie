bcdec6fa1dbe17574e59ba2b011c01a8aa61a2f3
Stefan Richter <sealor@users.noreply.github.com>
2022-11-14 22:25:56 +0100

    Increase version number to 3.0.0.dev4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='junkie',
-    version='3.0.0.dev3',
+    version='3.0.0.dev4',
     packages=setuptools.find_packages(exclude="test"),
     author='Stefan Richter',
     description='A dependency injection library for beginners',
